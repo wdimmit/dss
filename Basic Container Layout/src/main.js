965f931044467b29606bfdf34472df05f4cc8246
Will Dimmit <will@ultimatetrip.net>
2015-09-13 11:14:39 -0500

    Rewritten using array.prototype.map

--- Basic Container Layout/src/main.js
+++ Basic Container Layout/src/main.js
@@ -1,83 +1,56 @@
 //@program
+/*
+  Copyright 2011-2015 Marvell Semiconductor, Inc.
+  
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+  
+      http://www.apache.org/licenses/LICENSE-2.0
+      
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
 /* Skins and Styles */
-var redSkin = new Skin ({fill: 'red'});
-var greenSkin = new Skin ({fill: 'green'});
 var blueSkin = new Skin ({fill: 'blue'});
-var purpleSkin = new Skin ({fill: 'purple'});
-var yellowSkin = new Skin ({fill: 'yellow'});
+var greenSkin = new Skin ({fill: 'green'});
+var redSkin = new Skin ({fill: 'red'});
 var whiteSkin = new Skin ({fill: 'white'});
+var yellowSkin = new Skin ({fill: 'yellow'});
 
-var whiteStyle = new Style ({ font: '18px', color: 'white' });
-var blackStyle = new Style ({ font: '18px', color: 'black' });
+var blackStyle = new Style ({ font: '18px', color: 'black', horizontal: 'left' });
+var whiteStyle = new Style ({ font: '18px', color: 'white', horizontal: 'left'});
 
 
-/* Extra container definition */
-var containerFour = new Container({
-	left: 0, right: 0, bottom: 0, height: 75, skin: yellowSkin, active: true,
+
+var ContainerTemplate = Container.template(function ($) { return {
+	top: $.top, bottom: $.bottom, left: $.left, right: $.right, height: $.height, width: $.width, skin: $.skin,
 	contents: [
-		new Label({string: 'Behind 2 (4)', style: blackStyle}),
-		new Label({bottom: 5, string: '(Remove)', style: blackStyle})
-	],
-	behavior: Object.create(Behavior.prototype, {
-		onTouchEnded: { value: function(container, id, x, y, ticks) {
-			container.container.remove(containerFour);
-		}},
-	}),	
-});
+	           Text($, {top: 0, bottom: 0, left: 5, right: 0, string: $.string, style: $.style})
+       ]
+}})
 
 /* Main screen layout */
-var mainContainer = new Container({
-	left: 0, right: 0, top: 0, bottom: 0, 
-	active: false, skin: whiteSkin, name: 'mainCon',
-	contents: [
-		new Container( { top: 0, left: 0, right: 0, height: 100, skin: redSkin, name: 'containerOne', active: true,
-			contents: [
-			    new Label({top: 5, string: '(Add)', style: whiteStyle}),
-				new Label({string: 'Top and Side (1)', style: whiteStyle})						
-			],
-			behavior: Object.create(Behavior.prototype, {
-				onTouchEnded: { value: function(container, id, x, y, ticks) {
-					container.container.add(containerFour);
-				}},
-			}),	
-			
-		}),
-		new Container( { top: 0, bottom: 0, right: 0, width: 100, skin: greenSkin, active: true, name: 'containerTwo',
-			contents: [
-				new Column({
-					contents: [
-						new Label({string: '(Insert)', style: whiteStyle}),
-						new Label({string: '', style: whiteStyle}),
-						new Label({string: 'Top,', style: whiteStyle}),
-						new Label({string: 'Bottom', style: whiteStyle}),
-						new Label({string: 'Right', style: whiteStyle}),
-						new Label({string: '(2)', style: whiteStyle}),
-					]		
-				}),
-			],
-			behavior: Object.create(Behavior.prototype, {
-				onTouchEnded: { value: function(container, id, x, y, ticks) {
-					container.container.insert(containerFour, container.container.containerGreen);
-				}},
-			}),	
-		}),
-		new Container( { width: 160, height: 100, skin: blueSkin, active: true, name: 'containerThree',
-			contents: [
-			    new Label({top: 5, string: '(Color Parent)', style: whiteStyle}),
-				new Label({string: 'Height, Width (3)', style: whiteStyle})
-			],
-			behavior: Object.create(Behavior.prototype, {
-				onTouchEnded: { value: function(container, id, x, y, ticks) {
-					container.container.skin = purpleSkin;
-				}},
-			}),			
-		}), 
-	],
-});
+var mainContainer = Container.template(function ($) { return {
+	left: 0, right: 0, top: 0, bottom: 0, skin: whiteSkin, 
+	contents: $.containers.map(function(item) {
+			return new ContainerTemplate(item);
+		})	
+}});
 
 /* Application definition */
 application.behavior = {
 	onLaunch: function() { 
-		application.add(mainContainer);
+		var data = this.data = {containers: [
+             {top: 0, bottom: undefined, left: 0, right: 0, height: 100, width: undefined, skin: redSkin, string: 'Positioned By:\n\ntop, left, right, height', style: whiteStyle},
+             {top: 0, bottom: 0, left: undefined, right: 0, height: undefined, width:100, skin: greenSkin, string: 'Positioned By:\n\ntop, bottom, \nright, width', style: whiteStyle},
+             {top: undefined, bottom: 0, left: 0, right: 0, height: 75, width: undefined, skin: yellowSkin, string: 'Positioned By:\n\nbottom, left, right, height', style: blackStyle},
+             {top: undefined, bottom: undefined, left: undefined, right: undefined, height: 100, width: 160, skin: blueSkin, string: 'Positioned By:\n\nheight, width', style: whiteStyle}
+        ]}
+		application.add(new mainContainer(data));
 	}
 }
