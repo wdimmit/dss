e28bd65c66546e42b17f3d671074799e63169b95
Will Dimmit <will@ultimatetrip.net>
2015-09-14 18:00:23 -0500

    Code style fixups.

--- Basic Checkbox Sample/src/main.js
+++ Basic Checkbox Sample/src/main.js
@@ -1,32 +1,33 @@
 //@program
-var THEMEX = require('themes/flat/theme');
+var THEME = require('themes/flat/theme');
 var BUTTONS = require('controls/buttons');
 
-var whiteSkin = new Skin({fill:"white"});
-var blueSkin = new Skin ({fill: '#00F'});
+var blueSkin = new Skin ({fill: 'blue'});
 var greySkin = new Skin({fill: '#999'});
+var whiteSkin = new Skin({fill:'white'});
+
 var whiteStyle = new Style ({ font: '20px', color: 'white' });
 
-var myStatusBar = new Container({
+var MyStatusBar = new Container({
 	top: 5, left: 10, right: 10, height: 35, skin: greySkin, 
 	contents: [
 	          new Label({style: whiteStyle, name: 'statusLabel'})
       ]
 })
 
-var myCheckBoxTemplate = BUTTONS.LabeledCheckbox.template(function($){ return{
+var MyCheckBoxTemplate = BUTTONS.LabeledCheckbox.template(function($){ return{
 	top:50, bottom:50, left:50, right:50,
 	behavior: Object.create(BUTTONS.LabeledCheckboxBehavior.prototype, {
 		onSelected: { value:  function(checkBox){
-			myStatusBar.statusLabel.string = checkBox.buttonLabel.string + ' checked';
+			MyStatusBar.statusLabel.string = checkBox.buttonLabel.string + ' checked';
 		}},
 		onUnselected: { value:  function(checkBox){
-			myStatusBar.statusLabel.string = checkBox.buttonLabel.string + ' unchecked';
+			MyStatusBar.statusLabel.string = checkBox.buttonLabel.string + ' unchecked';
 		}}
 	})
 }});
 
-var mySimpleButtons = new Line({
+var MySimpleButtons = new Line({
 	left: 20, right: 20, bottom: 5, height: 50, active: true,
 	contents: [
 		new Container({left: 10, right: 10, skin: blueSkin, active: true,
@@ -54,7 +55,7 @@ var mySimpleButtons = new Line({
 	
 });
 
-var mainCon = new Column({left:0, right:0, top:0, bottom:0, skin: whiteSkin});
+var MainContainer = new Column({left:0, right:0, top:0, bottom:0, skin: whiteSkin});
 
 function checkTheBox(element, index, array) {
     element.distribute("setSelected", true, false);
@@ -63,13 +64,23 @@ function unCheckTheBox(element, index, array) {
     element.distribute("setSelected", false, false);
 }
 
-var checkbox = [];
-checkbox[0] = new myCheckBoxTemplate({name:"Hello"});
-checkbox[1] = new myCheckBoxTemplate({name:"World"});
-checkbox[2] = new myCheckBoxTemplate({name:"Click Me"});
+var checkbox = [
+	new MyCheckBoxTemplate({name:"Hello"}),
+	new MyCheckBoxTemplate({name:"World"}),
+	new MyCheckBoxTemplate({name:"Click Me"})
+];
 
-application.add(mainCon);
 
-mainCon.add(myStatusBar);
-for (var i = 0; i < 3; i++) mainCon.add(checkbox[i]);
-mainCon.add(mySimpleButtons);
+/* Application definition */
+application.behavior = {
+	onLaunch: function() { 
+		MainContainer.add(MyStatusBar);
+		
+		for (var i = 0; i < 3; i++) 
+			MainContainer.add(checkbox[i]);
+		
+		MainContainer.add(MySimpleButtons);
+		application.add(MainContainer);
+
+	}
+}
